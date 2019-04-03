b07740f3ea7f0f5d4ab675767a9836c0a3f4dddd
hua <289876247@qq.com>
2019-04-03 09:27:06 +0800

    initial version

---
+++ src/main/java/org/zechac/codegene/CodegeneApplication.java
@@ -0,0 +1,13 @@
+package org.zechac.codegene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegeneApplication.class, args);
+	}
+
+}
